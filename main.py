838ad0ca68365ee47b850ccc70b156df3b4e7e2f
Miropups <gasbir26@gmail.com>
2025-10-01 12:23:48 +0300

    v0.01
    выполнен весь функционал, поставленный в задачи на эту версию

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ def main(page: ft.Page):
     page.bgcolor = "transparent"
     page.window.bgcolor = "transparent"
     page.window.title_bar_hidden = True
-    #page.window.frameless = True
+    page.window.frameless = True
     page.window.always_on_top = True
-    page.window.width = 250
-    page.window.height = 150
+    page.window.ignore_mouse_events = True
+    
+    page.window.width = 200
+    page.window.height = 200
+    page.padding = 0
+    page.spacing=0
+
+    page.window.left = 1120
+    page.window.top = 720
+    
 
     page.add(
         ft.Image(
-            src="picture.webp",
+            src="1.png", #320x320
+            width=120,
+            height=120,
+            
             
         )
     )
